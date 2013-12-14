7d0b728fa08bc0131f14c5b8398023cd10a457cf
NuttySimple <jordang29@hotmail.com>
2013-12-14 08:03:33 -0800

    Implemented Step 1 - Game Piece Introductions

--- MGAProtoType/MGAViewController.m
+++ MGAProtoType/MGAViewController.m
@@ -11,6 +11,8 @@
 
 @interface MGAViewController ()
 
+@property (strong, nonatomic) UILabel *lbl_instruction;
+
 @property (strong, nonatomic) UIImageView *iv_map;
 
 @property (strong, nonatomic) UIImageView *iv_japanPlaceholder;
@@ -29,8 +31,6 @@
     CGPoint _targetPointOnMap;
     CGPoint _targetPointOfGamePiece;
     
-    NSString *_instruction;
-    
     UILabel *_lbl_instruction;
     
     // Various frames for map images
@@ -43,6 +43,9 @@
     CGRect _southKoreaGamePieceFrameDraggingStep;
     CGRect _northKoreaGamePieceFrameDraggingStep;
     CGRect _japanGamePieceFrameDraggingStep;
+    
+    NSArray *_gamePieceArray;
+    NSArray *_placeholderArray;
 }
 
 - (void)viewDidLoad
@@ -86,6 +89,8 @@
     self.iv_northKoreaGamePiece.frame = _northKoreaGamePieceFrameDraggingStep;
     self.iv_japanGamePiece.frame = _japanGamePieceFrameDraggingStep;
     
+    _gamePieceArray = [[NSArray alloc] initWithObjects:self.iv_northKoreaGamePiece, self.iv_southKoreaGamePiece, self.iv_japanGamePiece, nil];
+    _placeholderArray = [[NSArray alloc] initWithObjects:self.iv_northKoreaPlaceholder, self.iv_southKoreaPlaceholder, self.iv_japanPlaceholder, nil];
     
     // Add the map piece to the view. Order is important because the game pieces need to be on top of other pieces while dragging.
     [self.view addSubview:self.iv_map];
@@ -99,26 +104,44 @@
     [self.view addSubview:self.iv_japanGamePiece];
     
     // TODO: TEMP For now i have only programmed the dragging step. The Stage starts with the animated Intro step.
-    [self setupGamePiecesForDragging];
+//    [self setupGamePiecesForDragging];
+    [self setupStep1];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
     
-    _instruction = @"Show me where these go";
-    
     float labelHeight = 80.0f;
     CGRect labelFrame = CGRectMake(0.0, self.view.bounds.size.height, self.view.bounds.size.width, labelHeight);    // Put the view off the screen to the bottom
-    _lbl_instruction = [[UILabel alloc] initWithFrame:labelFrame];
-    [_lbl_instruction setBackgroundColor:[UIColor colorWithRed:175.0f/255.0f green:235.0f/255.0f blue:254.0f/255.0f alpha:1.0f]];
-    [_lbl_instruction setText:_instruction];
-    [_lbl_instruction setTextAlignment:NSTextAlignmentCenter];
-    [_lbl_instruction setTextColor:[UIColor whiteColor]];
-    [_lbl_instruction setFont:[UIFont boldSystemFontOfSize:36.0f]];
-    [self.view addSubview:_lbl_instruction];
+    self.lbl_instruction = [[UILabel alloc] initWithFrame:labelFrame];
+    [self.lbl_instruction setBackgroundColor:[UIColor colorWithRed:175.0f/255.0f green:235.0f/255.0f blue:254.0f/255.0f alpha:1.0f]];
+    [self.lbl_instruction setTextAlignment:NSTextAlignmentCenter];
+    [self.lbl_instruction setTextColor:[UIColor whiteColor]];
+    [self.lbl_instruction setFont:[UIFont boldSystemFontOfSize:36.0f]];
+    [self.view addSubview:self.lbl_instruction];
     
+//    // Animate the view into the screen coming up from the bottom, then disappearing again.
+//    void (^nextStep)(void) = ^(void) {
+//        [self setupStep1];
+//    };
+//    
+//    [self showInstructionWithText:_instruction completion:nextStep];
+    [self startStep1];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Instruction Label Instance Methods
+- (void)showInstructionWithText:(NSString *)text completion:(void (^)(void))completion {
     // Animate the view into the screen coming up from the bottom, then disappearing again.
-    labelFrame = CGRectMake(0.0, self.view.bounds.size.height - labelHeight, self.view.bounds.size.width, labelHeight);
+    [self.lbl_instruction setText:text];
+    
+    float labelHeight = self.lbl_instruction.frame.size.height;
+    CGRect labelFrame = CGRectMake(0.0, self.view.bounds.size.height - labelHeight, self.view.bounds.size.width, labelHeight);
     [UIView animateWithDuration:0.7
                           delay:0.0
                         options:UIViewAnimationOptionCurveEaseInOut
@@ -134,15 +157,10 @@
                                               _lbl_instruction.frame = newFrame;
                                           }
                                           completion:^(BOOL finished){
-                                              
+                                              if (completion)
+                                                  completion();
                                           }];
                      }];
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Step 1 (Intro) Instance Methods
@@ -157,6 +175,119 @@
     self.iv_southKoreaGamePiece.frame = _southKoreaPlaceholderFrameOnMap;
     self.iv_northKoreaGamePiece.frame = _northKoreaPlaceholderFrameOnMap;
     self.iv_japanGamePiece.frame = _japanPlaceholderFrameOnMap;
+    
+    [self.view sendSubviewToBack:self.iv_southKoreaGamePiece];
+    [self.view sendSubviewToBack:self.iv_northKoreaGamePiece];
+    [self.view sendSubviewToBack:self.iv_japanGamePiece];
+}
+
+- (void)introduceGamePiece:(MGAGamePiece *)gamePiece
+           withPlaceholder:(UIImageView *)placeholder
+                 withScale:(float)scale
+                  withText:(NSString *)text
+                completion:(void (^)(void))completion
+{
+    CGPoint originalCenterGamePiece = gamePiece.center;
+    CGPoint originalCenterPlaceholder = placeholder.center;
+    
+    [self.lbl_instruction setText:text];
+    
+    float labelHeight = self.lbl_instruction.frame.size.height;
+    CGRect newLabelFrame = CGRectMake(0.0, self.view.bounds.size.height - labelHeight, self.view.bounds.size.width, labelHeight);
+    
+    [UIView animateWithDuration:0.7
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         self.iv_map.alpha = 0.0f;
+                         
+                         for (MGAGamePiece *otherGamePiece in _gamePieceArray) {
+                             if (otherGamePiece != gamePiece) {
+                                 otherGamePiece.alpha = 0.0f;
+                             }
+                         }
+                         
+                         for (UIImageView *otherPlaceholder in _placeholderArray) {
+                             if (otherPlaceholder != placeholder) {
+                                 otherPlaceholder.alpha = 0.0f;
+                             }
+                         }
+                         
+                         gamePiece.center = self.view.center;
+                         gamePiece.transform = CGAffineTransformScale(gamePiece.transform, scale, scale);
+                         
+                         placeholder.center = self.view.center;
+                         placeholder.transform = CGAffineTransformScale(placeholder.transform, scale, scale);
+                         
+                         _lbl_instruction.frame = newLabelFrame;
+                     }
+                     completion:^(BOOL finished){
+                         CGRect newLabelFrame = CGRectMake(0.0, self.view.bounds.size.height, self.view.bounds.size.width, labelHeight);
+                         
+                         [UIView animateWithDuration:0.7
+                                               delay:5.0
+                                             options:UIViewAnimationOptionCurveEaseInOut
+                                          animations:^{
+                                              self.iv_map.alpha = 1.0f;
+                                              
+                                              for (MGAGamePiece *otherGamePiece in _gamePieceArray) {
+                                                  if (otherGamePiece != gamePiece) {
+                                                      otherGamePiece.alpha = 1.0f;
+                                                  }
+                                              }
+                                              
+                                              for (UIImageView *otherPlaceholder in _placeholderArray) {
+                                                  if (otherPlaceholder != placeholder) {
+                                                      otherPlaceholder.alpha = 1.0f;
+                                                  }
+                                              }
+                                              
+                                              gamePiece.center = originalCenterGamePiece;
+                                              gamePiece.transform = CGAffineTransformIdentity;
+                                              
+                                              placeholder.center = originalCenterPlaceholder;
+                                              placeholder.transform = CGAffineTransformIdentity;
+                                              
+                                              _lbl_instruction.frame = newLabelFrame;
+                                          }
+                                          completion:^(BOOL finished){
+                                              if (completion)
+                                                  completion();
+                                          }];
+                     }];
+}
+
+- (void)startStep1 {
+    // Of every game piece we create a completion block that will instruct
+    // the view control to introduce the next game piece. The last game piece
+    // has no completion handler.
+    
+    void (^nextStep)(void) = ^(void) {
+        [self startStep3];
+    };
+    
+    void (^showJapan)(void) = ^(void) {
+        [self introduceGamePiece:self.iv_japanGamePiece
+                 withPlaceholder:self.iv_japanPlaceholder
+                       withScale:1.25
+                        withText:@"This is Japan"
+                      completion:nextStep];
+    };
+    
+    void (^showSouthKorea)(void) = ^(void) {
+        [self introduceGamePiece:self.iv_southKoreaGamePiece
+                 withPlaceholder:self.iv_southKoreaPlaceholder
+                       withScale:2.0
+                        withText:@"This is South Korea"
+                      completion:showJapan];
+    };
+    
+    // We start with the first game piece in the stage. We hide the map at the same time.
+    [self introduceGamePiece:self.iv_northKoreaGamePiece
+             withPlaceholder:self.iv_northKoreaPlaceholder
+                   withScale:2.0
+                    withText:@"This is North Korea"
+                  completion:showSouthKorea];
 }
 
 
@@ -164,6 +295,22 @@
 
 
 #pragma mark - Step 3 (Dragging - On Map) Instance Methods
+- (void)startStep3 {
+    [UIView animateWithDuration:0.7
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseInOut
+                     animations:^{
+                         self.iv_southKoreaGamePiece.frame = _southKoreaGamePieceFrameDraggingStep;
+                         self.iv_northKoreaGamePiece.frame = _northKoreaGamePieceFrameDraggingStep;
+                         self.iv_japanGamePiece.frame = _japanGamePieceFrameDraggingStep;
+                     }
+                     completion:^(BOOL finished){
+                         [self setupGamePiecesForDragging];
+                         
+                         [self showInstructionWithText:@"Show me where these go" completion:nil];
+                     }];
+}
+
 - (void)setupGamePiecesForDragging {
     [self.iv_southKoreaGamePiece makeGamePieceDraggable];
     self.iv_southKoreaGamePiece.delegate = self;
